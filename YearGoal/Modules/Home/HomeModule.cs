f5d8a87015f1b5848e41eae718bd2d4b8d078ed4
pawelsawicz <pawel@sawicz.eu>
2014-02-21 00:20:54 +0100

    initialize project

---
+++ YearGoal/Modules/Home/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace YearGoal.Modules.Home
+{
+    public class HomeModule : NancyModule 
+    {
+        public HomeModule()
+        {
+           
+        }
+    }
+}
