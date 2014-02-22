d9eb6d75c581a52eeb606912b86278c0f15496e0
pawelsawicz <pawel@sawicz.eu>
2014-02-22 08:55:57 +0100

    add logn/register mocks

---
+++ YearGoal/Modules/Account/AccountModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace YearGoal.Modules.Account
+{
+    public class AccountModule : NancyModule
+    {
+        public AccountModule()
+        {
+            Get["/login"] = _ =>
+            {
+                return View["Login"];
+            };
+
+            Get["/register"] = _ =>
+            {
+                return View["Register"];
+            };
+        }
+    }
+}
