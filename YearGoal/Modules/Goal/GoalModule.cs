604d02e50eb08864a830cb6fd601e7531c4a5028
pawelsawicz <pawel@sawicz.eu>
2014-02-22 22:29:35 +0100

    add crud view mock for Goal dashboard

---
+++ YearGoal/Modules/Goal/GoalModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+namespace YearGoal.Modules.Goal
+{
+    public class GoalModule : NancyModule
+    {
+        
+        public GoalModule()
+        {
+            Get["/goal"] = _ =>
+            {
+                return View["Index"];
+            };
+        }
+    }
+}
