81abad230cedaf77e734a611cd113410addd428c
pawelsawicz <pawel@sawicz.eu>
2014-02-23 18:56:37 +0100

    add project test to solution

---
+++ YearGoal.Test/Goal/GoalTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using YearGoal.Data.Repository.Interface;
+using Moq;
+namespace YearGoal.Test.Goal
+{
+    [TestFixture]
+    public class GoalTest
+    {     
+
+        [SetUp]
+        public void SetUp()
+        {
+           
+        }
+
+        
+    }
+}
