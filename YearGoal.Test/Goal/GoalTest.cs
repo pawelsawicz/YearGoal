a7baf7f908cd8975e2cf34d7dad113d6b8413a8b
pawelsawicz <pawel@sawicz.eu>
2014-02-23 23:29:44 +0100

    add failed test for codereview, after all will be deleted

--- YearGoal.Test/Goal/GoalTest.cs
+++ YearGoal.Test/Goal/GoalTest.cs
@@ -6,18 +6,42 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using YearGoal.Data.Repository.Interface;
 using Moq;
+using FluentAssertions;
 namespace YearGoal.Test.Goal
 {
     [TestFixture]
     public class GoalTest
-    {     
+    {
+        private readonly IGoalService _goalService;
+        private readonly Mock<IGoalRepository> mockRepository;
 
         [SetUp]
         public void SetUp()
         {
+            var mockRepository = new Mock<IGoalRepository>();            
+        }       
+
+        [Test]
+        public void add_goal_by_user_stub()
+        {
+            //arrange   
+            CreateGoalViewModel model = new CreateGoalViewModel()
+            {
+                Name = "Test Goal"
+            };            
+            mockRepository.Setup(repository => repository.Add(new Data.Model.Goal()
+            {
+                Name = "Test Goal"
+            })).Verifiable();            
+            _goalService = new GoalService(mockRepository.Object);
+
+            //act
+            _goalService.AddGoal(model);
            
+            //assert
+            mockRepository.Verify(x => x.Add(new Data.Model.Goal() { Name = "Test Goal" }));
         }
 
-        
+
     }
 }
