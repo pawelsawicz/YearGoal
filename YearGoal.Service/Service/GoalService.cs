94f6e2b70028e5a271961a0d13a514cb415d5f0a
pawelsawicz <pawel@sawicz.eu>
2014-02-24 01:19:35 +0100

    add service layer

---
+++ YearGoal.Service/Service/GoalService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YearGoal.Service.Service.Interface;
+
+namespace YearGoal.Service.Service
+{
+    public class GoalService : IGoalService
+    {
+
+    }
+}
---
+++ YearGoal.Service/Service/Interface/IService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YearGoal.Service.Service.Interface
+{
+    public interface IService
+    {
+
+    }
+}
